5513b0d58b47139cd963e29b8534e53153ac45b6
Gijs Molenaar <gijsm@spotify.com>
2023-12-02 07:45:51 +0200

    add two methods

--- 2023/day2/run.py
+++ 2023/day2/run.py
@@ -1,4 +1,4 @@
-def doit(file: str, max_red: int, max_green: int, max_blue: int):
+def doit_without_returns(file: str, max_red: int, max_green: int, max_blue: int):
     valid_games = []
     for line in open(file).readlines():
         game_str, draw_str = line.split(":")
@@ -20,12 +20,39 @@ def doit(file: str, max_red: int, max_green: int, max_blue: int):
             valid_games.append(game)
     return valid_games
 
+
+def doit_return_cubes(file: str, max_red: int, max_green: int, max_blue: int):
+    invalid_games = []
+    games = []
+    for line in open(file).readlines():
+        split = line.split(":")
+        game = int(split[0][5])
+        games.append(game)
+        draws = split[1].strip().split(';')
+        print(game)
+        for draw in draws:
+            blue = red = green = 0
+            for cube in draw.split(','):
+                count, color = cube.strip().split()
+                if color == 'blue':
+                    blue += int(count)
+                elif color == 'red':
+                    red += int(count)
+                elif color == 'green':
+                    green += int(count)
+            if blue > max_blue or green > max_green or red > max_red:
+                invalid_games.append(game)
+            print(f"\t{blue=}\t{red=}\t{green=}")
+    valid_games = set(games) - set(invalid_games)
+    return list(valid_games)
+
+
 print("test")
-valid_games = doit('input_test.txt', max_red=12, max_green=13, max_blue=14)
+valid_games = doit_return_cubes('input_test.txt', max_red=12, max_green=13, max_blue=14)
 print(valid_games)
 print("should be 1, 2, and 5")
 
 print("------\nproduction")
-valid_games = doit('input.txt', max_red=12, max_green=13, max_blue=14)
+valid_games = doit_return_cubes('input.txt', max_red=12, max_green=13, max_blue=14)
 print(valid_games)
 print(sum(int(i) for i in valid_games))

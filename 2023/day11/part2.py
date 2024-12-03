1f4f3f858317fce9627893e96a787d9bbe5b76aa
Gijs Molenaar <gijsm@spotify.com>
2024-12-03 14:10:46 +0200

    day 11 2023

---
+++ 2023/day11/part2.py
@@ -0,0 +1,17 @@
+from numpy import array, all, where
+from itertools import combinations
+
+
+def f(p, inc=10):
+    a = array([list(line.strip()) for line in open(p, 'r').readlines()])
+    r = where(all(a == '.', axis=1))[0]
+    c = where(all(a == '.', axis=0))[0]
+    for u, v in combinations(list(zip(*where(a == '#'))), 2):
+        yield abs(u[0] - v[0]) + abs(u[1] - v[1])
+        yield sum([inc - 1 for big in r if min(u[0], v[0]) < big < max(u[0], v[0])])
+        yield sum([inc - 1 for big in c if min(u[1], v[1]) < big < max(u[1], v[1])])
+
+
+print(sum(f('test.txt', inc=10)))  # should be 1030
+print(sum(f('test.txt', inc=100)))  # should be 8410
+print(sum(f('input.txt', inc=1000000)))

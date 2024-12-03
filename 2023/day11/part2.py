03b3076c929508ad799485d52ac5acf33eedeeeb
Gijs Molenaar <gijsm@spotify.com>
2024-12-03 14:18:16 +0200

    cleanup

--- 2023/day11/part2.py
+++ 2023/day11/part2.py
@@ -6,12 +6,12 @@ def f(p, inc=10):
     a = array([list(line.strip()) for line in open(p, 'r').readlines()])
     r = where(all(a == '.', axis=1))[0]
     c = where(all(a == '.', axis=0))[0]
-    for u, v in combinations(list(zip(*where(a == '#'))), 2):
+    for u, v in combinations(zip(*where(a == '#')), 2):
         yield abs(u[0] - v[0]) + abs(u[1] - v[1])
-        yield sum([inc - 1 for big in r if min(u[0], v[0]) < big < max(u[0], v[0])])
-        yield sum([inc - 1 for big in c if min(u[1], v[1]) < big < max(u[1], v[1])])
+        yield sum([inc - 1 for b in r if min(u[0], v[0]) < b < max(u[0], v[0])])
+        yield sum([inc - 1 for b in c if min(u[1], v[1]) < b < max(u[1], v[1])])
 
 
 print(sum(f('test.txt', inc=10)))  # should be 1030
 print(sum(f('test.txt', inc=100)))  # should be 8410
-print(sum(f('input.txt', inc=1000000)))
+print(sum(f('input.txt', inc=1_000_000)))

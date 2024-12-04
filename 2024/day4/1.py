eea9cdef632b292a970da98ecd16fa412172548c
Gijs Molenaar <gijsm@spotify.com>
2024-12-04 09:31:39 +0200

    day4

---
+++ 2024/day4/1.py
@@ -0,0 +1,22 @@
+from numpy import array, diag, flip
+
+S = "'X', 'M', 'A', 'S'"
+
+def c(a):
+    return repr(a).count(S)
+
+def f(p):
+    a = array([list(l.strip()) for l in open(p).readlines()])
+    yield c(a)
+    yield c(flip(a, 1))
+    yield c(a.T)
+    yield c(flip(a.T, 1))
+    for i in range(a.shape[1] - 1, -a.shape[0], -1):
+        yield c(diag(a, i))
+        yield c(flip(diag(a, i)))
+        yield c(diag( flip(a), i))
+        yield c(flip(diag(flip(a), i)))
+
+
+print(sum(f('test2.txt')))  # should give 18
+print(sum(f('input.txt')))

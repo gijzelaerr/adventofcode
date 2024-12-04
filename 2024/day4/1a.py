b941800b311d3b16c16553a04d3d0e6b3a7e57be
Gijs Molenaar <gijsm@spotify.com>
2024-12-04 09:59:19 +0200

    alternative then

---
+++ 2024/day4/1a.py
@@ -0,0 +1,15 @@
+S = "XMAS"
+
+
+def f(p):
+    r = open(p).readlines()
+    d = {(y, x): r[y][x] for y in range(len(r)) for x in len(r[0]) - 1}
+    k = [-1, 0, 1]
+
+    for y, x in d:
+        for dy, dx in [(dy, dx) for dy in k for dx in k if (dx != 0 or dy != 0)]:
+            yield "".join(d.get((y + dy * i, x + dx * i), ".") for i in range(len(S))) == S
+
+
+print(sum(f("test2.txt")))
+print(sum(f("input.txt")))

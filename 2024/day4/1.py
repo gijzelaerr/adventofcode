d051f7dd39817aa441dc62022af6fae0dd500fe4
Gijs Molenaar <gijsm@spotify.com>
2024-12-04 09:41:07 +0200

    better

--- 2024/day4/1.py
+++ 2024/day4/1.py
@@ -2,8 +2,10 @@ from numpy import array, diag, flip
 
 S = "'X', 'M', 'A', 'S'"
 
+
 def c(a):
     return repr(a).count(S)
+
 
 def f(p):
     a = array([list(l.strip()) for l in open(p).readlines()])
@@ -12,10 +14,11 @@ def f(p):
     yield c(a.T)
     yield c(flip(a.T, 1))
     for i in range(a.shape[1] - 1, -a.shape[0], -1):
+        print(flip(diag(a, i)))
         yield c(diag(a, i))
         yield c(flip(diag(a, i)))
-        yield c(diag( flip(a), i))
-        yield c(flip(diag(flip(a), i)))
+        yield c(diag(flip(a, 1), i))
+        yield c(flip(diag(flip(a, 1), i)))
 
 
 print(sum(f('test2.txt')))  # should give 18
